8ad02cabb5df810ba3ba2abe9927f50343a8862d
Dinko Azema <rocket4ce@gmail.com>
2025-09-06 10:13:56 -0400

    fix: Update page controller test to assert correct response message

--- test/game_eight_web/controllers/page_controller_test.exs
+++ test/game_eight_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GameEightWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Página funcionando correctamente"
   end
 end
