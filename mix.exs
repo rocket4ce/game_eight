4318230ecf974aee33fff3024e4e83c6be0d2a90
Dinko Azema <rocket4ce@gmail.com>
2025-09-06 09:36:27 -0400

    Add MCP configuration, proxy, and dependencies for Tidewave and Igniter

--- mix.exs
+++ mix.exs
@@ -40,6 +40,8 @@ defmodule GameEight.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:tidewave, "~> 0.4", only: [:dev]},
+      {:igniter, "~> 0.6", only: [:dev, :test]},
       {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
